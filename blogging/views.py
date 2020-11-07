912255f31ad3f11184e208248a3c79d29eb61408
Kyle Rhyner <klrhyner@u.washington.edu>
2020-11-06 20:30:12 -0800

    implemented blog post view

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
+from django.template import loader
+from blogging.models import Post
 # Create your views here.
 
 def stub_view(request, *args, **kwargs):
@@ -12,3 +14,17 @@ def stub_view(request, *args, **kwargs):
         body += 'kwargs:\n'
         body += '\n'.join(['\t%s: %s' % i for i in kwargs.items()])
     return HttpResponse(body, content_type='text/plain')
+
+# responsibility of a view is to accept a request
+def list_view(request):
+    # query publihsed posts
+    published = Post.objects.exclude(published_date__exact=None)
+    # sort the query in reverse order
+    posts = published.order_by('-published_date')
+    # the following 3 comments so common, theres a shortcut
+    # and that shortcut is the return render()
+    # template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    # body = template.render(context)
+    # return HttpResponse(body, content_type='text/html')
+    return render(request, 'list.html', context)
