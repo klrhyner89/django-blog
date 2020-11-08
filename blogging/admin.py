7cc3a9cc3a61d939dd53ad59787dfd8b04e13d94
Kyle Rhyner <klrhyner@u.washington.edu>
2020-11-07 19:17:55 -0800

    finished assignment-07 by allowing categorizing when creating a post

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+# Define an inline class here
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+               CategoryInline,
+               ]
+    exclude = ('posts',)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+               CategoryInline,
+               ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
