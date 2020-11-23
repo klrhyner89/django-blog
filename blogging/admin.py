cd13549cda56441bffb47fb212e3e4d4e8a5297c
Kyle Rhyner <klrhyner@u.washington.edu>
2020-11-22 21:02:33 -0800

    applied black

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
 from blogging.models import Post, Category
+
 # Register your models here.
 
 # Define an inline class here
 class CategoryInline(admin.TabularInline):
     model = Category.posts.through
 
+
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [
-               CategoryInline,
-               ]
-    exclude = ('posts',)
+        CategoryInline,
+    ]
+    exclude = ("posts",)
+
 
 class PostAdmin(admin.ModelAdmin):
     inlines = [
-               CategoryInline,
-               ]
+        CategoryInline,
+    ]
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
