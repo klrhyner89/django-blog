ac4a0b001adbfafe47c01ffbdf10a49fbce482b2
Kyle Rhyner <klrhyner@u.washington.edu>
2020-11-28 15:53:37 -0800

    add missed files

--- blogging/views.py
+++ blogging/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from django.template import loader
-from blogging.models import Post
+from blogging.models import Post, Category
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from blogging.serializers import UserSerializer, PostSerializer, CategorySerializer
 
 # Create your views here.
 
@@ -39,3 +43,25 @@ class PostDetailView(DetailView):
     #     post = published.get(pk=self.get_object().pk)
     #     context = {'object': post}
     #     return render(request, 'blogging/detail.html', context)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint thta allows users to be viewed or edited
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
